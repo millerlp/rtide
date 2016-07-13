e1c56d507bf9288864d0ce012cb423832a4e6191
joethorley <joe@poissonconsulting.ca>
2016-07-13 09:20:14 -0700

    fix subset test

--- tests/testthat/test-subset.R
+++ tests/testthat/test-subset.R
@@ -2,6 +2,6 @@ context("subset")
 
 test_that("subset works", {
   h <- rtide::harmonics
-  expect_identical(subset(h, h$Station$Station[2])$Station,
+  expect_equal(subset(h, h$Station$Station[2])$Station,
                    h$Station[2,,drop = FALSE])
 })
