1789ffcbb79fd949bc155e373ba565805a5ca992
joethorley <joe@poissonconsulting.ca>
2016-07-13 15:29:30 -0700

    Derived from XTide

--- R/data.R
+++ R/data.R
@@ -2,8 +2,7 @@
 #'
 #' A object of class tide_harmonics providing tidal harmonic data for US stations.
 #'
-#' The tidal harmonic data was originally derived from XTide (http://www.flaterco.com/xtide/)
-#' by Luke Miller (https://github.com/millerlp/Misc_R_scripts).
+#' The data is derived from XTide (http://www.flaterco.com/xtide/).
 "harmonics"
 
 #' Monterey Tide Height Data
