2e834f7bf4b1ac788d02e2abdcd10725567095bf
Joe Thorley <joe@poissonconsulting.ca>
2017-05-09 14:17:36 -0700

    fix license and url

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #'
 #' A object of class tide_harmonics providing tidal harmonic data for US stations.
 #'
-#' Converted from harmonics-dwf-20151227-free, NOAA web site data processed by David Flater for XTide.
+#' Converted from \url{ftp://ftp.flaterco.com/xtide/harmonics-dwf-20151227-free.tar.bz2}, NOAA web site data processed by David Flater for XTide.
 "harmonics"
 
 #' Monterey Tide Height Data
