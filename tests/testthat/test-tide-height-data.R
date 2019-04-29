e2906cb0fd351dac0dd48b62688a95bb246574d2
Joe Thorley <joe@poissonconsulting.ca>
2019-04-29 12:37:21 -0700

    fix note

--- tests/testthat/test-tide-height-data.R
+++ tests/testthat/test-tide-height-data.R
@@ -21,8 +21,6 @@ test_that("tide_height_data predictions", {
 })
 
 test_that("tide_height_data checks", {
-  library(lubridate)
-
   data <- data.frame(Station = "Monterey, Monterey Harbor, California",
                      DateTime = ISOdate(2015,1,1,10,tz = "PST8PDT"),
                      stringsAsFactors = FALSE)
@@ -32,20 +30,18 @@ test_that("tide_height_data checks", {
   expect_error(tide_height_data(data), "data already has 'TideHeight' column")
 
   data$TideHeight <- NULL
-  year(data$DateTime) <- 1699
+  dtt_year(data$DateTime) <- 1699L
   expect_error(tide_height_data(data), "years are outside harmonics range")
 })
 
 test_that("tide_height_data tz", {
-  library(lubridate)
-
   data <- data.frame(Station = "Monterey, Monterey Harbor, California",
                      DateTime = ISOdate(2015,1,1,10,tz = "PST8PDT"),
                      stringsAsFactors = FALSE)
 
   data2 <- data
-  data2$DateTime <- dtt_adjust_tz(data2$DateTime, tz = "EST")
+  data2$DateTime <- dttr::dtt_adjust_tz(data2$DateTime, tz = "EST")
 
-  expect_equal(tide_height_data(data), tide_height_data(data2))
+  expect_equal(tide_height_data(data)$TideHeight, tide_height_data(data2)$TideHeight)
 })
 
