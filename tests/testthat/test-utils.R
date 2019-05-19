2cc8789fbd79b10e65a143941f23f0d5b7c100bd
joethorley <joe@poissonconsulting.ca>
2019-05-19 06:28:49 -0700

    fix test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -3,5 +3,5 @@ context("utils")
 test_that("datetimes and seconds work", {
   start <- ISOdate(1970,1,1,0)
   expect_identical(datetime2seconds(seconds2datetime(datetime2seconds(start))), 0)
-  expect_identical(datetime2seconds(seconds2datetime(datetime2seconds(dttr::dtt_add_seconds(start, 3L)))), 3)
+  expect_identical(datetime2seconds(seconds2datetime(datetime2seconds(dttr2::dtt_add_seconds(start, 3L)))), 3)
 })
