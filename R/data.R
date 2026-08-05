4fa3242af4d53055be10b4400f3568778e8bcbb8
Luke Miller <millerlp@gmail.com>
2026-08-05 00:20:50 -0700

    Update harmonic data source comment

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #'
 #' A object of class tide_harmonics providing tidal harmonic data for US stations.
 #'
-#' Converted from harmonics-dwf-20151227-free, NOAA web site data processed by David Flater for XTide.
+#' Converted from harmonics-dwf-20261228-free.tar.xz, NOAA web site data processed by David Flater for XTide.
 "harmonics"
 
 #' Monterey Tide Height Data
