a20530888dc460285cf5ef08e1ffbc6bfaa96b99
Luke Miller <millerlp@gmail.com>
2026-08-04 22:31:41 -0700

    Add some comments

--- data-raw/harmonics.R
+++ data-raw/harmonics.R
@@ -3,9 +3,10 @@ library(rtide)
 
 rm(list = ls())
 
-# From https://github.com/millerlp/Misc_R_scripts
-load("data-raw/Harmonics_20120302.Rdata")
+# After running 'data-raw/tide_harmonics_parse.R' on a new 
+# text-format harmonics database file from Xtide. 
+load("data-raw/Harmonics_20251228.Rdata")
 
-harmonics <- rtide:::tide_harmonics(harms)
+harmonics <- rtide:::tide_harmonics(harms) # convert to tide_harmonics object
 
-use_data(harmonics, overwrite = TRUE)
+use_data(harmonics, overwrite = TRUE) # create a new harmonics.rda file in /data
