0b64909175a41074bbd6ab6adc1d5ae563cdbc0d
Luke Miller <millerlp@gmail.com>
2026-08-06 20:59:06 -0700

    Pre-process text file to deal with non-ASCII chars
    The text-converted tcd data base file is now processed first to remove characters that don't conform to the basic ASCII character set (i.e. accented UTF-8 characters).

--- data-raw/tide_harmonics_parse.R
+++ data-raw/tide_harmonics_parse.R
@@ -7,7 +7,7 @@
 ###############################################################################
 # This is essentially a one-time use script to call the read_harmonicsfile.R
 # functions and parse the tidal harmonics file from XTide. The resulting data
-# for the ~637 (now 1293 circa 2025) reference stations can be stored in a Rdata file for quick
+# for the ~637 (now ~1286 circa 2025) reference stations can be stored in a Rdata file for quick
 # loading and use in tide predictions.
 # The harmonics file must be a text format, not the binary
 # tcd format that is generally distributed with XTide. To generate this text
@@ -19,10 +19,33 @@
 # Load the functions from read_harmonicsfile.R
 source('./read_harmonicsfile.R')
 
+# Start this process by dealing with accented characters in
+# the text file. We'll convert some of them to their base
+# characters without the accent mark, and others will end up
+# getting processed into NAs.
+
+# Read the original UTF-8 file
+txt <- readLines("./harmonics-20251228.txt", encoding = "UTF-8")
+
+# Remove accents, e.g. é -> e, ñ -> n, ü -> u
+txt2 <- iconv(txt, from = "UTF-8", to = "ASCII//TRANSLIT")
+
+# Convert from ASCII to Latin-1
+txt <- iconv(txt, from = "ASCII", to = "latin1")
+
+# Write the file explicitly using Latin-1 encoding
+con <- file("./harmonics-20251228_cleaned.txt", open = "w", encoding = "latin1")
+writeLines(txt, con)
+close(con)
+
+# Some of the station names with problematic UTF-8 characters will
+# end up with a station name entry of 'NA', so we'll lose them
+# in future steps.
 
 # Create a connection to the harmonics text file for reading.
 # fid = file('./harmonics-20141224.txt', open = 'rt') # old version
-fid = file('./harmonics-20251228.txt', open = 'rt') # newer version
+fid = file('./harmonics-20251228_cleaned.txt', open = 'rt') # newer version
+
 
 # Call the read_harmonicsfile function (found in read_harmonicsfile.R) to parse
 # the harmonics file into a usable format.
@@ -34,8 +57,9 @@ close(fid)
 ##############
 # Now remove the current speed stations from the data set to avoid potential
 # ambiguities in finding tide stations by name. This should leave only
-# the tide Reference stations (Subordinate stations are not included in the
-# tide harmonics tcd database.
+# the tide Reference stations. (Subordinate stations are not included in the
+# tide harmonics tcd database) We also filter out any stations
+# that ended up with "NA" as a station name
 harms2 = list(name = harms$name, speed = harms$speed,
 		startyear = harms$startyear,
 		equilarg = harms$equilarg,
@@ -51,8 +75,11 @@ harms2 = list(name = harms$name, speed = harms$speed,
 		A = NULL,
 		kappa = NULL)
 
+
+
 for (i in 1:length(harms$units)) {
-	if (harms$units[i] == 'feet') {
+	# if (harms$units[i] == 'feet') {
+	if (harms$units[i] == 'feet' & harms$station[i] != 'NA') {
 		harms2$station = c(harms2$station, harms$station[i])
 		harms2$stationIDnumber = c(harms2$stationIDnumber,
 				harms$stationIDnumber[i])
@@ -73,4 +100,7 @@ harms = harms2
 # Save the results to a Rdata file, since there's no need to re-parse the
 # harmonics file once you've done it once.
 save(harms, file = 'Harmonics_20251228.Rdata')
+
+# Afterwards, to make a final harmonics file formatted for use
+# with rtide, you'll need to run the 'harmonics.R' script.
 
