82cb3e3ca20cd9ca0191545e13debb87797726ce
Luke Miller <millerlp@gmail.com>
2026-08-06 23:17:57 -0700

    Updated processing, renaming MLLW column
    Changing MLLW column to TideHeight since the values in this column are both low and high tides, while MLLW refers to a chart datum.

--- data-raw/brandywine.R
+++ data-raw/brandywine.R
@@ -6,17 +6,17 @@ library(rtide)
 
 rm(list = ls())
 
-# From https://tidesandcurrents.noaa.gov/noaatidepredictions/NOAATidesFacade.jsp?Stationid=9413450
+# From https://tidesandcurrents.noaa.gov/noaatidepredictions.html?id=8555889&units=metric&bdate=20160713&edate=20160715&timezone=LST/LDT&clock=24hour&datum=MLLW&interval=hilo&action=dailychart
 
 brandywine <- read_tsv("data-raw/8555889.txt", skip = 13)
 brandywine <- mutate(brandywine, Station = tide_stations("Brandywine"))
 
-brandywine <- select(brandywine, Station, Date, Time, MLLW = Pred)
+brandywine <- select(brandywine, Station, Date, Time, TideHeight = Pred)
 brandywine <- unite(brandywine, DateTime, Date, Time, sep = " ")
 
 brandywine <- mutate(brandywine,
-  DateTime = dtt_date_time(DateTime, tz = "EST5EDT"),
-  MLLW = rtide:::ft2m(MLLW)
+  DateTime = dtt_date_time(DateTime, tz = "America/New_York"),
+  TideHeight= rtide:::ft2m(TideHeight)
 )
 
 use_data(brandywine, overwrite = TRUE)
--- data-raw/monterey.R
+++ data-raw/monterey.R
@@ -6,17 +6,17 @@ library(rtide)
 
 rm(list = ls())
 
-# From https://tidesandcurrents.noaa.gov/noaatidepredictions/NOAATidesFacade.jsp?Stationid=9413450
+# From https://tidesandcurrents.noaa.gov/noaatidepredictions.html?id=9413450&units=standard&bdate=20160713&edate=20160714&timezone=LST/LDT&clock=24hour&datum=MLLW&interval=hilo&action=dailychart
 
 monterey <- read_tsv("data-raw/9413450.txt", skip = 13)
 monterey <- mutate(monterey, Station = tide_stations("Monterey,"))
 
-monterey <- select(monterey, Station, Date, Time, MLLW = Pred)
+monterey <- select(monterey, Station, Date, Time, TideHeight = Pred)
 monterey <- unite(monterey, DateTime, Date, Time, sep = " ")
 
 monterey <- mutate(monterey,
-  DateTime = dtt_date_time(DateTime, tz = "PST8PDT"),
-  MLLW = rtide:::ft2m(MLLW)
+  DateTime = dtt_date_time(DateTime, tz = "America/Los_Angeles"),
+  TideHeight = rtide:::ft2m(TideHeight)
 )
 
 use_data(monterey, overwrite = TRUE)
