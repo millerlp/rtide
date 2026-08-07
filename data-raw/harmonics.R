e190d1084566012334a36fe06822ea0fcfc79ab7
Luke Miller <millerlp@gmail.com>
2026-08-06 19:02:45 -0700

    Clarify the workflow

--- data-raw/harmonics.R
+++ data-raw/harmonics.R
@@ -3,8 +3,10 @@ library(rtide)
 
 rm(list = ls())
 
-# After running 'data-raw/tide_harmonics_parse.R' on a new 
-# text-format harmonics database file from Xtide. 
+# After running 'data-raw/tide_harmonics_parse.R' on a new
+# text-format harmonics database file from Xtide, run this script
+# to further process the harmonics Rdata file into the final
+# harmonics format used by rtide.
 load("data-raw/Harmonics_20251228.Rdata")
 
 harmonics <- rtide:::tide_harmonics(harms) # convert to tide_harmonics object
